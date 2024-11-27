15205e751b94db8d36c05ca359fe6e5966c2690a
Владислав Васин <VasinVladislav94@gmail.com>
2024-11-27 10:57:32 +0300

    Добавьте файлы проекта.

---
+++ HomeWorkCSharp-1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCSharp_1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.1. Вывести на экран число с точностью до сотых.
+            Random rnd = new Random();  //Создание объекта для генерации чисел.
+            double number = rnd.NextDouble() + rnd.Next(0, 9);   //Сложение рандомного натурального числа и рандомного целого числа.
+            string value = string.Format("{0:f}", number);   //Преревод числительной переменной в строку и форматирование дробного числа.
+            Console.WriteLine(value);
+        }
+    }
+}
